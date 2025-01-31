8259c8b7a0efc367795b021a33573f154e040773
Raja Kumar <rajkumar.dongiri@betalen.in>
2025-01-31 12:48:55 +0530

    Add project files.

---
+++ Study_Hub_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Study_Hub_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
